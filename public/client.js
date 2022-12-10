f8a98d9b2b8312958e3a1ebbd8575d1549cc0ca1
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2022-12-10 11:48:24 -0600

    created submitMath function and working on sending it to server

--- public/client.js
+++ public/client.js
@@ -2,9 +2,39 @@ $(document).ready(onReady);
 
 function onReady() {
     $('.submitButton').on('click', submitCalculation);
+    $('#clearButton').on('click', clearInputs);
 }
 
 let numberOne = '';
 let numberTwo = '';
+let operator = '';
 
 
+function submitMath() {
+
+
+    calculatorInfo = {
+        numberOne: numberOne,
+        type: operator,
+        numberTwo: numberTwo,
+        result: 'empty',
+    }
+
+    console.log(calculatorInfo);
+    $.ajax({
+        method: 'POST',
+        url: '/submit',
+        data: calculatorInfo,
+    }).then(function (results) {
+        console.log(results);
+    })
+    fetchResults();
+
+    numberOne = '';
+    numberTwo = '';
+    operator = '';
+}
+
+function clearInputs() {
+    
+}
