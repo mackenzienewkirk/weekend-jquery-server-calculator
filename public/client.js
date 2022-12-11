f23b7bd9b8b9226f115e4d1d624acb37b1062e3d
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2022-12-11 12:03:27 -0600

    writing functions to get and render the calculator info from the server

--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
 $(document).ready(onReady);
 
 function onReady() {
+    console.log('This is the onReady');
     $('.submitButton').on('click', submitCalculation);
     $('#clearButton').on('click', clearInputs);
 }
@@ -11,7 +12,7 @@ let operator = '';
 
 
 function submitMath() {
-
+console.log('a submitMath function to send the inputs to the servers');
 
     calculatorInfo = {
         numberOne: numberOne,
@@ -28,13 +29,55 @@ function submitMath() {
     }).then(function (results) {
         console.log(results);
     })
-    fetchResults();
+    getResults();
 
     numberOne = '';
     numberTwo = '';
     operator = '';
 }
 
+function inputCompiler() {
+    if (operator === '') {
+        numberOne = numberOne + $(this).attr('id');
+        console.log(numberOne);
+
+        $('.calculator').empty();
+        $('.calculator').append(numberOne);
+    } else {
+        numberTwo += $(this).attr('id')
+
+        console.log(numberTwo);
+        $('.calculator').empty();
+        $('.calculator').append(numberTwo);
+
+    }
+}
+
+//a function to get the results of the calculator back from the server
+function getResults() {
+    $.ajax({
+        method: 'GET',
+        url: '/submit',
+    }).then(function (history) {
+        console.log(history);
+        renderResults(history);
+    })
+}
+
+function renderResults() {
+    $('#history').empty();
+    let last = history[history.length - 1];
+    for (let i = history.length - 1; i >= 0; i--) {
+        $('#history').append(`
+            <li> ${history[i].numberOne} ${history[i].type} ${history[i].numberTwo} = ${history[i].result}  </li>
+        `);
+        console.log('appended');
+        $('.calculator').text(`${last.result}`);
+    }
+
+}
+
+
 function clearInputs() {
-    
+
 }
