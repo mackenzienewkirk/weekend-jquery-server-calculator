97350303a71874dfa5a96ac10bee22ae1da9d2d1
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2022-12-11 15:03:17 -0600

    server side POST and get routes to revieve and send back info for the client side

--- server/server.js
+++ server/server.js
@@ -6,14 +6,28 @@ const PORT = 5000;
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Variables to use throughout the server sides functions.
 let equation;
 let result;
 let history = [];
 
-function calculator() {
+app.post('/submit', (req, res) => {
+    equation = req.body;
+    calculator(equation);
+    //a response to signal the client side recieved the calculation.
+    res.sendStatus(201);
+});
+
+app.get('/submit', (req, res) => {
+    res.send(history);
+})
+
+
+
+function calculator(object) {
     let firstNumber = Number(object.firstNumber);
     let secondNumber = Number(object.secondNumber);
-//math logic for the calculator
+//math logic for the calculator.
     if (object.type === '+') {
         object.result = firstNumber + secondNumber;
         toString(object.result);
@@ -23,7 +37,6 @@ function calculator() {
     console.log(history);
     return object;
 }
-
 
 app.listen(PORT, () => {
     console.log(`listening on http://localhost:${PORT}`);
