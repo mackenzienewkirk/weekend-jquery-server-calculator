6cefa8015791d7a822fa88a30370731c92558508
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2022-12-12 14:24:34 -0600

    updated files for cleaner work process

--- server/public/client.js
+++ server/public/client.js
@@ -2,104 +2,56 @@ $(document).ready(onReady);
 
 //an onReady function when the browser is ready.
 function onReady() {
-    console.log('This is the onReady');
-    clickHandlers();
-    getResults();
+    console.log('this is the onReady function!');
+    $('.operatorButton').on('click', onOperatorClick)
+    $('#equalsButton').on('click', onEqualsClick);
+    $('#clearButton').on('click', resetCalculator);
 }
 
-//a clickHandlers function to active the buttons and inputs on the browser.
-function clickHandlers() {
-    $('.submitButton').on('click', submitMath);
-    $('#clearButton').on('click', clearInputs);
-    $('#addButton').on('click', typeOfOperator);
-    $('#subtractButton').on('click', typeOfOperator);
-    $('#multiplyButton').on('click', typeOfOperator);
-    $('#divideButton').on('click', typeOfOperator);
-    submitMath();
-}
-
-let numberOne = '';
-let numberTwo = '';
-let operator = '';
-
-//a submitMath function to send input info to the server.
-function submitMath() {
-console.log('a submitMath function to send the inputs to the servers');
-
-//an object of the calculator info to send to the server to be put in the history array.
-let calculatorInfo = {
-        numberOne: Number(numberOne),
-        numberTwo: Number(numberTwo),
-        type: operator,
-    }
-
+function getAndRenderCalculations() {
     $.ajax({
-        method: 'POST',
-        url: '/submit',
-        data: calculatorInfo,
-    }).then(function (results) {
-        console.log(results);
-    })
-    getResults();
+        method: 'GET',
+        url: '/calculations'
+    }).then((calculationsFromServer) => {
+    let mostRecentCalc = calculationsFromServer[calculationsFromServer.length - 1];
+    $('#mostRecentCalculation').text(mostRecentCalc.result)
 
+    $('#allTheCalculations').empty();
+    for (let aCalc of calculationsFromServer) {
+        $('#allTheCalculations').append(`
+        <li>${aCalc.numOne} ${aCalc.operator} ${aCalc.numTwo} = ${aCalc.result}</li>
+        `)
+    }
+    })
 }
 
 // //a function to determine which operator was used in between the number inputs.
 function typeOfOperator() {
-    if (operator === '') {
-    numberOne.slice(0,-1);
+    operator = $(this).text();
+}
+
+function onEqualsClick() {
+    //get values from number inputs//
+    let numOne = $('#numberOne').val();
+    let numTwo = $('#numberTwo').val();
+
+    let newCalculation = {
+        numOne: numOne,
+        numTwo: numTwo,
+        operator: operator
+    }
     
-    numberOne = $('.calculator').text();
-    console.log(numberOne);
-    }
-    operation = $(this).text();
-    console.log(operation);
-    return operator;
-}
-
-//a function to append and empty the input values in the calculator.
-function inputCompiler() {
-    if (operator === '') {
-    numberOne = numberOne + $(this).attr('id');
-    console.log(numberOne);
-
-    $('.calculator').empty();
-    $('.calculator').append(numberOne);
-    } else {
-    numberTwo += $(this).attr('id')
-
-    console.log(numberTwo);
-    $('.calculator').empty();
-    $('.calculator').append(numberTwo);
-
-    }
-}
-
-//a function to get the results of the calculator back from the server
-function getResults() {
     $.ajax({
-        method: 'GET',
-        url: '/submit',
-    }).then(function (history) {
-    console.log(history);
-    renderResults(history);
+    method: 'POST',
+    url: '/calculations',
+    data: newCalculation
+    }).then((response) => {
+    clearInputs();
+    getAndRenderCalculations();
     })
 }
 
-//a function to render the most recent results recieved from the server.js.
-function renderResults() {
-    $('#history').empty();
-    let last = history[history.length - 1];
-    for (let i = history.length - 1; i >= 0; i--) {
-    $('#history').append(`
-    <li> ${history[i].numberOne} ${history[i].type} ${history[i].numberTwo} = ${history[i].result}  </li>
-    `);
-    console.log('appended');
-    $('.calculator').text(`${last.result}`);
-    }
 
-}
-console.log('appended');
 
 //a function to clear all elements of the calculator when the clear 'c' button is clicked.
 function clearInputs() {
--- server/server.js
+++ server/server.js
@@ -8,52 +8,45 @@ app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-let equation;
-let result;
-let history = [];
+let calculations = []
 //Variables to use throughout the server sides functions.
 
-app.post('/submit', (req, res) => {
-    equation = req.body;
-    calculator(equation);
-    //a response to signal the client side recieved the calculation.
-    res.sendStatus(201);
-});
-
-//a function to calculate the given information.
-function calculator(object) {
-    let firstNumber = Number(object.firstNumber);
-    let secondNumber = Number(object.secondNumber);
-//math logic for the calculator.
-    if (object.type === '+') {
-        object.result = firstNumber + secondNumber;
-        toString(object.result);
-    }
-    if (object.type === '-') {
-        object.result = firstNumber - secondNumber;
-        toString(object.result);
-    }
-    if (object.type === 'x') {
-        object.result = firstNumber * secondNumber;
-        toString(object.result);
-
-    }
-    if (object.type === '/') {
-        object.result = firstNumber / secondNumber;
-        toString(object.result);
-
-    history.push(calculator);
-    console.log(history);
-    
-    }return object;
-}
-
-//sending the calculated numbers in the history array back to client.js.
-app.get('/submit', (req, res) => {
-    console.log('GET /submit sent to the client side');
-    res.send(history);
+app.get('/calculations', (req, res) => {
+    console.log('get /calculations!');
+    res.send(calculations);
 })
 
+app.post('/calculations', (req, res) => {
+    console.log('post /calculations!');
+
+    let numOne = Number(req.body.numOne);
+    let numTwo = Number(req.body.numTwo);
+    let operator = req.body.operator;
+
+    let result;
+    if (operator === '+') {
+        result = numOne + numTwo;
+    } else if (operator === '-') {
+        result = numOne - numTwo;
+    } else if (operator === '/') {
+        result = numOne / numTwo;
+    } else if (operator === '*') {
+      result = numOne * numTwo;
+    }
+
+    let finalCalculation = {
+        numOne: numOne,
+        numTwo: numTwo,
+        operator: operator,
+        result: result
+    }
+
+    calculations.push(finalCalculation)
+
+    res.sendStatus(201);
+})
+
+
 app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
+    console.log(`Server is running on port, http://localhost:${PORT}!`);
 })
